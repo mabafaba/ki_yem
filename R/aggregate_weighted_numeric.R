72d50fe0f27c811460d8e6d5422c536df9d08dc2
Martin B <barner.mar@gmail.com>
2018-11-09 18:28:09 +0200

    export

--- R/aggregate_weighted_numeric.R
+++ R/aggregate_weighted_numeric.R
@@ -7,6 +7,7 @@
 #' @param aggregate.to vector of group names to which to aggregate
 #' @details bla
 #' @value data frame with aggregated values and group names
+#' @export
 aggregate_weighted_numeric <- function(x,aggregate.to) {
   if(length(x)!=length(aggregate.to)){stop("x and aggregate.to must have the same lenght")}
 x %>% split(aggregate.to) %>% sapply(mean)
